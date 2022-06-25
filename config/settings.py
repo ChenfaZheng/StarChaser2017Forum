3c8469b8c80b999b318e622e7b840e95b6b53a2b
Chenfa Zheng <zhengcf@mail.bnu.edu.cn>
2022-06-25 20:44:14 +0800

    set email_use_ssl

--- config/settings.py
+++ config/settings.py
@@ -196,7 +196,8 @@ try:
     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
     EMAIL_HOST = env('EMAIL_HOST')
     EMAIL_PORT = env('EMAIL_PORT')
-    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+    # EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+    EMAIL_USE_SSL = env('EMAIL_USE_SSL')
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 except:

6d0e2e2d92c8d43cb78b1fe1d3e34aab71717929
Chenfa Zheng <zhengcf@mail.bnu.edu.cn>
2022-06-25 14:07:28 +0800

    report model change

--- reports/models.py
+++ reports/models.py
@@ -37,10 +37,10 @@ class Review(models.Model):
     )
     review = models.CharField(max_length=1000)
     date = models.DateTimeField(auto_now_add=True)
-    # author = models.ForeignKey(
-    #     get_user_model(), 
-    #     on_delete=models.CASCADE, 
-    # )
+    author = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE, 
+    )
 
     def __str__(self):
         return self.review

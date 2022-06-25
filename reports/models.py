1a7868e204d37e3eb6b5cffbed2dcfa3d3bd4f8c
Chenfa Zheng <zhengcf@mail.bnu.edu.cn>
2022-06-26 06:32:54 +0800

    update report uploader

--- reports/models.py
+++ reports/models.py
@@ -18,7 +18,7 @@ class Report(models.Model):
     uploader = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
-        default=get_user_model(), # default is the current user
+        default=get_user_model().get_username,
     )
 
 
